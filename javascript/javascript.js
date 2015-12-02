dd53b4096bb85894fc4282a61c688b7fd00de2e5
Brandon Chang <brandon@Brandons-MacBook-Pro-2.local>
2015-12-02 00:54:35 -0500

    changed color, created mobile, changed menu active logic

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,17 @@
+    var pageArray = [];
 $(document).ready(function(){
     var navArray = [];
     var lastPage = 1;
     var currentPage = 1;
     var scrolled = false;
+    var $windowHeight = $(window).innerHeight();
+    var $scrollTop = 0;
     
 	$('.icon').each (function(){
 		navArray.push('#'+this.id);
 	});
+    
+    assignPage();
     
     for(i=0; i<= navArray.length-1; i++){
 		$(navArray[i]).click(function(){	
@@ -19,24 +24,54 @@ $(document).ready(function(){
         });
     }
 
+    function assignPage(){
+        for( i = 0; i < navArray.length - 1; i++){
+            pageArray[i] = $( '#section' + (i+1) ).offset();
+        }
+    }
+
     $( window ).scroll(function() {
         scrolled = true;
     });
 
     function calcPage(){
-        if ( scrolled == true ){
+        if ( scrolled === true ){
             console.log( 'doing work' );
             $scrollTop = $(document).scrollTop();
-            $windowHeight = $(window).innerHeight();
-            page_offset = ( $scrollTop + $windowHeight * 0.5 ) % ( $windowHeight / 2 );
-            currentPage = ( ( ( $scrollTop - page_offset ) + $windowHeight ) / $windowHeight ); //I wrote this at 5:27am. Don't ask me how, but it works.
+            //currentPage = ( ( ( $scrollTop - page_offset ) + $windowHeight ) / $windowHeight ); //I wrote this at 5:27am. Don't ask me how, but it works.
+            //currentPage = Math.ceil( currentPage ); 
+            
+            currentPage = checkPage();
 
-            currentPage = Math.ceil( currentPage ); 
             if(lastPage != currentPage){
                 checkNav(currentPage);
             } 
             lastPage = currentPage;  
             scrolled = false;
+        }
+    }
+
+    $(window).resize(function(){
+        var $windowHeight = $(window).innerHeight();
+        assignPage();
+
+    })
+
+    function checkPage(){
+        for( i = 0; i <= pageArray.length; i++ ){
+            tempPage = pageArray[i].top;
+            the_scroll = $scrollTop + ( $windowHeight / 2 );
+            if( window.innerWidth <= 880 ){
+                console.log( tempPage );
+                the_scroll = the_scroll + 50;
+            }
+            if( i === 5 ){
+                return 6;
+            }else{
+                if( the_scroll >= tempPage && the_scroll < pageArray[i+1].top ){
+                    return i + 1;
+                }
+            }
         }
     }
 
@@ -51,8 +86,8 @@ $(document).ready(function(){
     
     checkNav(currentPage);
     
-    $('header').click(function(){
-        $('header').toggleClass('m-active');
+    $('#menuIcon').click(function(){
+        $('#iconText').toggleClass('m-active');
     });
     
 });
